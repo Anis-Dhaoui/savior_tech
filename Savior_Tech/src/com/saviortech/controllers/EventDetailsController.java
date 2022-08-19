797830dde8b74d37c5891df869889f9a6360a052
Anis-Dhaoui <freeconcept6@gmail.com>
2022-08-19 18:02:06 +0100

    Event detail interface prepared

--- Savior_Tech/src/com/saviortech/controllers/EventDetailsController.java
+++ Savior_Tech/src/com/saviortech/controllers/EventDetailsController.java
@@ -6,19 +6,46 @@ package com.saviortech.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
- * 
+ *
  * @author freec
  */
 public class EventDetailsController implements Initializable {
 
     @FXML
-    private Label eventId;
+    private ImageView phImage;
+    @FXML
+    private Text phTitle;
+    @FXML
+    private Text phDescription;
+    @FXML
+    private Text phStartDate;
+    @FXML
+    private Text phEndDate;
+    @FXML
+    private Text phStatus;
+    @FXML
+    private Text phLocation;
+    @FXML
+    private Text phCategory;
+    @FXML
+    private Text phPrice;
+    @FXML
+    private Text phOrgonizer;
+    @FXML
+    private Text phNbPart;
+    @FXML
+    private Text phMaxPart;
+    @FXML
+    private HBox eventId;
 
     /**
      * Initializes the controller class.
@@ -26,10 +53,13 @@ public class EventDetailsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }   
-    
-    void setLabel(String title){
-        eventId.setText(title);
+    }    
+
+    @FXML
+    private void participate(ActionEvent event) {
     }
     
+        void setLabel(String title){
+        phTitle.setText(title);
+    }
 }
