aeab5bdb28a207c187771d53a105ef3cf2a8a698
MARWEN009 <marwenzakraoui03@gmail.com>
2022-09-07 15:28:05 +0100

    gestion de publication

---
+++ Savior_Tech/src/com/saviortech/services/ServicePublication.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.saviortech.services;
+
+import com.saviortech.models.Publication;
+import com.mysql.cj.x.protobuf.Mysqlx;
+import com.mysql.cj.xdevapi.PreparableStatement;
+import com.saviortech.utils.DataSource;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.sql.Connection;
+import java.util.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.WritableImage;
+import javax.imageio.ImageIO;
+import javax.swing.DefaultListModel;
+
+public class ServicePublication implements IService<Publication> {
+
+    private Connection cnx = DataSource.getInstance().getCnx();
+
+    @Override
+    public void ajouter(Publication o) {
+        Date actuelle = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String date = dateFormat.format(actuelle);
+
+        String dc = date;
+        System.err.println(dc);
+        try {
+            String req = "INSERT INTO publication (titre,description,image,DATE,IDUTILISATEUR) values (?,?,?,?,?)";
+            PreparedStatement ps = cnx.prepareStatement(req);
+            ps.setString(1, o.getTitre());
+            ps.setString(2, o.getDescription());
+            ps.setString(3, o.getImage());
+            ps.setString(4, dc);
+            ps.setInt(5, o.getIdUt());
+            ps.executeUpdate();
+            System.out.println("Ajoutée!");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void modifier(Publication o) {
+
+        try {
+            String req = "UPDATE publication SET description=? where idPublication=?";
+            PreparedStatement ps = cnx.prepareStatement(req);
+            
+            ps.setString(1, o.getDescription());
+            
+
+            ps.setInt(2, o.getIdPublication());
+            ps.executeUpdate();
+            System.out.println("Modifiée!");
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    @Override
+    public void supprimer(Publication o) {
+        try {
+            String req = "DELETE FROM publication where idPublication=?";
+            PreparedStatement ps = cnx.prepareStatement(req);
+            ps.setInt(1, o.getIdPublication());
+            ps.executeUpdate();
+            System.out.println("supprimée!");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public List<Publication> afficher() {
+        List<Publication> pub = new ArrayList<>();
+        try {
+
+            String req = "SELECT TITRE,DESCRIPTION,IMAGE,DATE,NOMPRENOM,IDPUBLICATION\n"
+                    + "FROM publication,utilisateur\n"
+                    + "WHERE utilisateur.IDUTILISATEUR = publication.IDUTILISATEUR "
+                    + "ORDER BY DATE DESC;";
+            PreparedStatement ps = cnx.prepareStatement(req);
+            ResultSet res = ps.executeQuery();
+        
+
+            while (res.next()) {
+                pub.add(new Publication(res.getString(1), res.getString(2), res.getString(3), res.getString(4), res.getString(5), res.getInt(6)));
+
+            }
+
+            System.out.println("Puplication récupérées !");
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return pub;
+    }
+
+}
---
+++ Savior_Tech/src/com/saviortech/views/ViewPublicationController.java
@@ -0,0 +1,161 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.saviortech.views;
+
+import com.jfoenix.controls.JFXButton;
+import com.saviortech.models.Publication;
+import com.saviortech.models.Commentaire;
+import com.saviortech.models.Reaction;
+import com.saviortech.services.ServiceCommentaire;
+import com.saviortech.services.ServicePublication;
+import com.saviortech.services.ServiceReaction;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Marwen
+ */
+public class ViewPublicationController implements Initializable {
+
+    @FXML
+    private Label showTitre;
+    @FXML
+    private Label showDescription;
+    @FXML
+    private ScrollPane scrolPaneCom;
+    @FXML
+    private GridPane gridPaneCom;
+
+    ServiceCommentaire sc = new ServiceCommentaire();
+    ServicePublication sp = new ServicePublication();
+    ServiceReaction sr = new ServiceReaction();
+
+    @FXML
+    private TextField desComAjouter;
+    @FXML
+    private Button AjouterDes;
+    @FXML
+    private JFXButton modifierPub;
+    @FXML
+    private JFXButton supprimerPub;
+    @FXML
+    private HBox hboxModifier;
+    @FXML
+    private TextField textModif;
+    @FXML
+    private JFXButton jaime;
+    @FXML
+    private JFXButton jaimePas;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    public void setShowPublication(Publication pub) {
+        
+        showTitre.setText(pub.getTitre());
+        showDescription.setText(pub.getDescription());
+
+        List<Commentaire> coms = sc.afficher(pub.getIdPublication());
+        System.err.println(coms.size());
+
+        int column = 0;
+        int row = 1;
+        try {
+
+            for (int i = 0; i < coms.size(); i++) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("itemCommentaire.fxml"));
+                AnchorPane anchorPane = fxmlLoader.load();
+
+                ItemCommentaireController itemComController = fxmlLoader.getController();
+                itemComController.setData(coms.get(i));
+
+                if (column == 1) {
+                    column = 0;
+                    row++;
+                }
+                gridPaneCom.add(anchorPane, column++, row); //(child,column,row)
+                gridPaneCom.setMargin(anchorPane, new Insets(10));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    private void onClickAjouter(ActionEvent event) {
+        if (desComAjouter.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "le champ est vide!");
+        } else {
+
+            sc.ajouter(new Commentaire(desComAjouter.getText(), null, 3, HomeController.idPub));
+            desComAjouter.clear();
+            desComAjouter.setPromptText("Ecrivez un commentaire");
+            JOptionPane.showMessageDialog(null, "Commentaire ajoutée !");
+              
+        }
+       
+    }
+
+    @FXML
+    private void onClickModifier(ActionEvent event) {
+       hboxModifier.setVisible(true);
+       textModif.setText(showDescription.getText());
+    }
+
+    @FXML
+    private void onClickSupprimer(ActionEvent event) {
+        int response = JOptionPane.showConfirmDialog(null, "Do you want to continue?", "Confirm",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (response == JOptionPane.NO_OPTION) {
+            System.out.println("No button clicked");
+        } else if (response == JOptionPane.YES_OPTION) {
+           sp.supprimer(new Publication(HomeController.idPub));
+        } else if (response == JOptionPane.CLOSED_OPTION) {
+            System.out.println("JOptionPane closed");
+        }
+    }
+
+    @FXML
+    private void onModifier(ActionEvent event) {
+       
+       sp.modifier(new Publication(HomeController.idPub,textModif.getText()));
+       hboxModifier.setVisible(false);
+       JOptionPane.showMessageDialog(null, "Modifieé");
+     
+    }
+
+    @FXML
+    private void onClickJaime(ActionEvent event) {
+        sr.ajouter(new Reaction("jaime", HomeController.idUtilisateur,HomeController.idPub));
+    }
+
+    @FXML
+    private void onClickJaimePas(ActionEvent event) {
+         sr.ajouter(new Reaction("jaimepas", HomeController.idUtilisateur,HomeController.idPub));
+    }
+
+}
