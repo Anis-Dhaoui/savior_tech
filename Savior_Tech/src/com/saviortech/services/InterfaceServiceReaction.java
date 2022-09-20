a5fcf1ab6a0965583fe5bb56d1beabddf652b3e2
MARWEN009 <marwenzakraoui03@gmail.com>
2022-09-20 12:18:29 +0100

    correction les erreurs

---
+++ Savior_Tech/src/com/saviortech/services/InterfaceServiceReaction.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.saviortech.services;
+
+import java.util.List;
+
+/**
+ *
+ * @author freec
+ */
+public interface InterfaceServiceReaction<T> {
+
+    public void ajouter(T o);
+
+//    public void modifier(T o);
+//
+//    public void supprimer(T o);
+//
+    public List<T> afficher(int id);
+}
